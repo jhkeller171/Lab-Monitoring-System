bbfa7bb7a81d83b09fc9c9630572f65e6199932a
jhkeller171 <119902101+jhkeller171@users.noreply.github.com>
2023-09-29 18:57:55 -0400

    Sound Sensor POC done

---
+++ SoundSensorPOC/SoundSensorPOC.ino
@@ -0,0 +1,17 @@
+const int soundPin = A0;
+
+void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);//set ledPin as OUTPUT
+  Serial.begin(9600); //initialize the serial communication as 9600 bps
+}
+
+void loop(){
+  int value = analogRead(soundPin);
+  Serial.println(value);
+
+  if (value > 600){
+    digitalWrite(LED_BUILTIN,HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN,LOW);
+  }
+}
